fef60030ba87ea871dc94a435487fa1a96775d10
Kim Ongchangco <155955124+ongchangco@users.noreply.github.com>
2025-06-10 14:23:36 +0800

    FINALIZED SALES DATABASE

--- dbEditor.py
+++ dbEditor.py
@@ -4,35 +4,34 @@ import uuid
 import bcrypt
 
 def edit_database():
-    connectionPath = os.path.join("db/salesdb/daily/sales_d2025", "jul_2025.db")
+    connectionPath = os.path.join("db/salesdb", "sales_yearly.db")
     connection = sqlite3.connect(connectionPath)
     cursor = connection.cursor()
 
     # ADD COLUMN
     #cursor.execute("ALTER TABLE products_on_hand ADD COLUMN image BLOB")
-    
+
     # EDIT CELL
     # cursor.execute("UPDATE apr SET quantity_sold = ? WHERE product_id = ?;",("13-05-25","C004"))  
-
     # DELETE ENTRY
     #cursor.execute("DELETE FROM ingredients WHERE inventory_id = 'IN014'")
-    
+
     # DELETE COLUMN
     #cursor.execute("ALTER TABLE ingredients DROP COLUMN 'C011'")
-    
+
     # DELETE TABLE
     # cursor.execute("DROP TABLE IF EXISTS sales_now")
     # for i in range(6, 10): 
     #     table_name = f"d{i}"
     #     cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
-    
+
     # EDIT TABLE NAME
     #cursor.execute("ALTER TABLE this_month RENAME TO apr")
     # for i in range(1, 10):  # d1 to d9
     #     old_name = f"d{i}"
     #     new_name = f"d{str(i).zfill(2)}"  # zero-pads single digits to 2 digits
     #     cursor.execute(f"ALTER TABLE {old_name} RENAME TO {new_name}")
-    
+
     # Wastage
     # cursor.execute("""
     # CREATE TABLE IF NOT EXISTS wastage_record (
@@ -44,31 +43,52 @@ def edit_database():
     #     dec_date DATE
     # )
     # """)
+
+    # Sales Yearly
+    # cursor.execute(f"""
+    #         CREATE TABLE IF NOT EXISTS sales_y2025 (
+    #             inv_id TEXT PRIMARY KEY,
+    #             inv_desc TEXT,
+    #             quantity_sold INTEGER,
+    #             price REAL,
+    #             sales_total REAL GENERATED ALWAYS AS (quantity_sold * price) STORED
+    #         )
+    # """)
     
-    # sales
-    for i in range(1, 10):  # d01-d09
-        table_name = f"d0{i}"
-        cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS {table_name} (
-                inv_id TEXT PRIMARY KEY,
-                inv_desc TEXT,
-                quantity_sold INTEGER,
-                price REAL,
-                sales_total REAL GENERATED ALWAYS AS (quantity_sold * price) STORED
-            )
-    """)
-        
-    for i in range(10, 31):  # d10-d30
-        table_name = f"d{i}"
-        cursor.execute(f"""
-            CREATE TABLE IF NOT EXISTS {table_name} (
-                inv_id TEXT PRIMARY KEY,
-                inv_desc TEXT,
-                quantity_sold INTEGER,
-                price REAL,
-                sales_total REAL GENERATED ALWAYS AS (quantity_sold * price) STORED
-            )
-        """)
+    # for i in range(1, 10):  # d01-d09
+    #     table_name = f"d0{i}"
+    #     cursor.execute(f"""
+    #         CREATE TABLE IF NOT EXISTS {table_name} (
+    #             inv_id TEXT PRIMARY KEY,
+    #             inv_desc TEXT,
+    #             quantity_sold INTEGER,
+    #             price REAL,
+    #             sales_total REAL GENERATED ALWAYS AS (quantity_sold * price) STORED
+    #         )
+    # """)
+    
+    # for i in range(10, 31):  # d10-d30
+    #     table_name = f"d{i}"
+    #     cursor.execute(f"""
+    #         CREATE TABLE IF NOT EXISTS {table_name} (
+    #             inv_id TEXT PRIMARY KEY,
+    #             inv_desc TEXT,
+    #             quantity_sold INTEGER,
+    #             price REAL,
+    #             sales_total REAL GENERATED ALWAYS AS (quantity_sold * price) STORED
+    #         )
+    #     """)
+    # months = ("jan","feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec")
+    # for i in range(12):  # d01-d09
+    #     cursor.execute(f"""
+    #         CREATE TABLE IF NOT EXISTS {months[i]} (
+    #             inv_id TEXT PRIMARY KEY,
+    #             inv_desc TEXT,
+    #             quantity_sold INTEGER,
+    #             price REAL,
+    #             sales_total REAL GENERATED ALWAYS AS (quantity_sold * price) STORED
+    #         )
+    # """)
     
     # inv_dynamic
     # cursor.execute("""
